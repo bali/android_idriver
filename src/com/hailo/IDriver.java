8f508f99382bff3a53cc12d681da03d8f8ceb402
Harri Smatt <harri.smatt@hailocab.com>
2013-10-07 01:52:50 +0100

    Basic functionality implemented, assigning to Dave

--- src/com/hailo/IDriver.java
+++ src/com/hailo/IDriver.java
@@ -2,8 +2,21 @@
 
 package com.hailo;
 
-import android.app.Activity;
+import com.hailo.fragment.Driver5;
 
-public class IDriver extends Activity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+
+public class IDriver extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.idriver);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, new Driver5(), "TESTING!").commit();
+    }
 
 }
--- src/com/hailo/fragment/Driver5.java
+++ src/com/hailo/fragment/Driver5.java
@@ -2,8 +2,22 @@
 
 package com.hailo.fragment;
 
+import com.hailo.R;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Driver5 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_driver5, null);
+        ((TextView)view).setText("TESTING!");
+        return view;
+    }
 
 }
